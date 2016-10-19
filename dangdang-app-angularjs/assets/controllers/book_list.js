16898226bb1d23fcb27fb236937c327b2b4f63bb
zhangyize <202127027@qq.com>
2016-10-19 19:01:08 +0800

    s_er

--- dangdang-app-angularjs/assets/controllers/book_list.js
+++ dangdang-app-angularjs/assets/controllers/book_list.js
@@ -1,17 +1,17 @@
-app.controller('bookListController', ['$scope','$commonService','$routeParams',function($scope,c_s,r_p) {
+app.controller('bookListController', ['$scope','$commonService','$routeParams',function($scope,c_ser,r_p) {
     $scope.booksData = [];
-    c_s.getData('ertong',function(res){
+    c_ser.getData('ertong',function(res){
       console.log(res);
       console.log('数据获取成功');
       $scope.booksData =res.data;
     });
-    c_s.getData('lishi',function(res){
+    c_ser.getData('lishi',function(res){
       $scope.booksData =res.data;
     });
-    c_s.getData('dongman',function(res){
+    c_ser.getData('dongman',function(res){
       $scope.booksData =res.data;
     });
-    c_s.getData('qingchunwenxue',function(res){
+    c_ser.getData('qingchunwenxue',function(res){
       $scope.booksData =res.data;
     });
 }]);
