8ba1db1c0cb0f40953d1686fa94530ec6a12e04e
zhangyize <202127027@qq.com>
2016-10-19 15:56:56 +0800

    url

--- dangdang-app-angularjs/assets/api/service.js
+++ dangdang-app-angularjs/assets/api/service.js
@@ -2,7 +2,7 @@ app.factory('commonService',['$http',function($http){
   var service ={};
   service.getData =function(type,callBack){
     $http({
-      url:'../../data/dangdang'+type+'.json',
+      url:'/dangdang-app-angularjs/data/dangdang'+type+'.json',
       method:'get'
     })
     .then(function(res){
