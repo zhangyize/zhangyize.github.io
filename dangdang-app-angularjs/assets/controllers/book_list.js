2f013a08b5396d1deace690ef9e0f1c23d64451b
zhangyize <202127027@qq.com>
2016-10-19 18:52:09 +0800

    dushuju

--- dangdang-app-angularjs/assets/controllers/book_list.js
+++ dangdang-app-angularjs/assets/controllers/book_list.js
@@ -1,8 +1,17 @@
-app.controller('bookListController', ['$scope', 'bookListController',function($scope,b_lc) {
+app.controller('bookListController', ['$scope','$commonService','$routeParams',function($scope,c_s,r_p) {
     $scope.booksData = [];
-    b_lc.getData('ertong',function(res){
+    c_s.getData('ertong',function(res){
       console.log(res);
       console.log('数据获取成功');
-      $scope.booksData =res.data.data;
+      $scope.booksData =res.data;
+    });
+    c_s.getData('lishi',function(res){
+      $scope.booksData =res.data;
+    });
+    c_s.getData('dongman',function(res){
+      $scope.booksData =res.data;
+    });
+    c_s.getData('qingchunwenxue',function(res){
+      $scope.booksData =res.data;
     });
 }]);
